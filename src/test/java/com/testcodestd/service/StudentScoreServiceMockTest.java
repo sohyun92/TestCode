bea1b9156861e36acd107e31f8bda3a2809688cb
0830thgus@naver.com <0830thgus@naver.com>
2025-05-11 19:44:10 +0900

    성적 저장 로직 검증 테스트 코드 작성

--- src/test/java/com/testcodestd/service/StudentScoreServiceMockTest.java
+++ src/test/java/com/testcodestd/service/StudentScoreServiceMockTest.java
@@ -32,4 +32,72 @@ public class StudentScoreServiceMockTest {
         );
 
     }
+
+    @Test
+    @DisplayName("성적 저장 로직 검증 / 60점 이상인경우")
+    public void saveScroeMockTest(){
+        //given 평균점수가 60점 이상인경우
+        StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+        StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
+        StudentFailRepositroy studentFailRepositroy = Mockito.mock(StudentFailRepositroy.class);
+
+        StudentScoreService studentScoreService = new StudentScoreService(
+                studentScoreRepository,
+                studentPassRepository,
+                studentFailRepositroy
+        );
+
+        String givenStudentName ="sohyun";
+        String givenExam = "testExam";
+        Integer givenKoreaScore =80;
+        Integer givenEnglishScore =100;
+        Integer givenMathScore =60;
+        //when
+        studentScoreService.saveScore(
+                givenStudentName,
+                givenExam,
+                givenKoreaScore,
+                givenEnglishScore,
+                givenMathScore
+        );
+
+        //then
+        Mockito.verify(studentScoreRepository,Mockito.times(1)).save(Mockito.any()); //1번 실행이 되어야함
+        Mockito.verify(studentPassRepository,Mockito.times(1)).save(Mockito.any());  //1번 실행이 되어야함
+        Mockito.verify(studentFailRepositroy,Mockito.times(0)).save(Mockito.any()); //0번 실행이 되어야함
+    }
+
+
+    @Test
+    @DisplayName("성적 저장 로직 검증 / 60점 미만인경우")
+    public void saveScroeMockTest2(){
+        //given 평균점수가 60점 이상인경우
+        StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+        StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
+        StudentFailRepositroy studentFailRepositroy = Mockito.mock(StudentFailRepositroy.class);
+
+        StudentScoreService studentScoreService = new StudentScoreService(
+                studentScoreRepository,
+                studentPassRepository,
+                studentFailRepositroy
+        );
+        String givenStudentName ="sohyun";
+        String givenExam = "testExam";
+        Integer givenKoreaScore =40;
+        Integer givenEnglishScore =40;
+        Integer givenMathScore =60;
+        //when
+        studentScoreService.saveScore(
+                givenStudentName,
+                givenExam,
+                givenKoreaScore,
+                givenEnglishScore,
+                givenMathScore
+        );
+
+        //then
+        Mockito.verify(studentScoreRepository,Mockito.times(1)).save(Mockito.any()); //1번 실행이 되어야함
+        Mockito.verify(studentPassRepository,Mockito.times(0)).save(Mockito.any());
+        Mockito.verify(studentFailRepositroy,Mockito.times(1)).save(Mockito.any()); //1번 실행이 되어야함
+    }
 }
